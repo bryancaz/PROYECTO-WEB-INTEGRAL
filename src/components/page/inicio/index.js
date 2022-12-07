e928c793ab170fa124eb7bbdd66843e9b1929bdf
bryancaz <103477342+bryancaz@users.noreply.github.com>
2022-12-07 00:12:48 -0600

    Add the src folder

---
+++ src/components/page/inicio/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Link } from "react-router-dom";
+import Portada from "images/inicio.jpg";
+
+export default function Inicio() {
+    return (
+        <div className="inicio">   
+            <Link to="/">
+            <h1></h1>
+            </Link>
+            <Link to="/productos">
+            <h1></h1>
+            </Link>
+            <img src={Portada} alt=""/>
+        </div>
+    )
+}
---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBxxXzp5cbCy37E8stVZW5Og8USbEKYN6I",
+  authDomain: "tienda-c31b4.firebaseapp.com",
+  projectId: "tienda-c31b4",
+  storageBucket: "tienda-c31b4.appspot.com",
+  messagingSenderId: "570511519283",
+  appId: "1:570511519283:web:12fd2a55f2722fae09bd46"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
